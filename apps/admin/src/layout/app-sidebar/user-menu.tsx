1461be53d54af8ef5fbc2567c074afb59fbb96d6
Jarvis <100921416+JarvisMarkDown@users.noreply.github.com>
2026-05-08 11:50:30 +0800

    feat: 增加创作者指南入口

--- apps/admin/src/layout/app-sidebar/user-menu.tsx
+++ apps/admin/src/layout/app-sidebar/user-menu.tsx
@@ -197,6 +197,7 @@ function ContributorUserMenu() {
     const currentUser = useCurrentUser();
     const site = useBrowseSite();
     const siteUrl = site.data?.site.url ?? "";
+    const boardGuidanceUrl = "https://www.predictionmarkets.org/creator-on-boarding-guide/";
 
     return (
         <DropdownMenu>
@@ -231,6 +232,12 @@ function ContributorUserMenu() {
                     <a href={siteUrl} target="_blank" rel="noopener noreferrer">
                         <LucideIcon.ExternalLink />
                         <UserMenuItem.Label>View site</UserMenuItem.Label>
+                    </a>
+                </UserMenuItem>
+                <UserMenuItem>
+                    <a href={boardGuidanceUrl} target="_blank" rel="noopener noreferrer">
+                        <LucideIcon.Compass />
+                        <UserMenuItem.Label>Onboard guidance</UserMenuItem.Label>
                     </a>
                 </UserMenuItem>
                 <DropdownMenuSeparator />
