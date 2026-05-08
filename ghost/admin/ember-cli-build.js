210d0afff77f483c698cc47efaa65aae10936da2
Jarvis <100921416+JarvisMarkDown@users.noreply.github.com>
2026-05-08 17:15:57 +0800

    feat: 测试依赖引用

--- ghost/admin/ember-cli-build.js
+++ ghost/admin/ember-cli-build.js
@@ -269,7 +269,8 @@ module.exports = function (defaults) {
     if (app.env === 'development' || app.env === 'test') {
         // pull dynamic imports into the assets folder so that they can be lazy-loaded in tests
         // also done in development env so http://localhost:4200/tests works
-        app.import('node_modules/@tryghost/koenig-lexical/dist/koenig-lexical.umd.js', {outputFile: 'ghost/assets/koenig-lexical/koenig-lexical.umd.js'});
+        // app.import('node_modules/@tryghost/koenig-lexical/dist/koenig-lexical.umd.js', {outputFile: 'ghost/assets/koenig-lexical/koenig-lexical.umd.js'});
+        app.import('https://test.predictionmarkets.org/assets/koeing/tryghost-koenig-lexical/dist/koenig-lexical.umd.js', {outputFile: 'ghost/assets/koenig-lexical/koenig-lexical.umd.js'});
     }
 
     return app.toTree();
