15b3002f0c67d81ce465b21bacb53df8b56f6fc4
Jarvis <100921416+JarvisMarkDown@users.noreply.github.com>
2026-06-15 14:20:23 +0800

    fix: 修复mysql外键引用问题

--- ghost/core/core/server/data/migrations/versions/6.25/2026-03-31-20-31-19-drop-nullable-on-automated-emails-email-design-setting-id.js
+++ ghost/core/core/server/data/migrations/versions/6.25/2026-03-31-20-31-19-drop-nullable-on-automated-emails-email-design-setting-id.js
@@ -1,3 +1,4 @@
 const {createDropNullableMigration} = require('../../utils');
 
-module.exports = createDropNullableMigration('automated_emails', 'email_design_setting_id');
+module.exports = createDropNullableMigration('automated_emails', 'email_design_setting_id', {disableForeignKeyChecks: true});
+
